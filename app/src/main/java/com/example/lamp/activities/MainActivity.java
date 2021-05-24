583f66edafef579588dc2e6ef3d19e96990b6e19
Vitaly <vitaliy.saviour@gmail.com>
2021-05-24 18:31:15 +0300

    imageview to radiobutton

--- app/src/main/java/com/example/lamp/activities/MainActivity.java
+++ app/src/main/java/com/example/lamp/activities/MainActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import com.example.lamp.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView buttonWhiteOn;
-    private ImageView buttonYellowOff;
-    private ImageView buttonBlueOff;
-    private ImageView buttonPurpleOff;
-    private ImageView buttonRedOff;
+    private RadioButton buttonWhiteOn;
+    private RadioButton buttonYellowOff;
+    private RadioButton buttonBlueOff;
+    private RadioButton buttonPurpleOff;
+    private RadioButton buttonRedOff;
     private ImageView buttonOn;
 
     private int fonColor;
@@ -26,73 +27,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonWhiteOn = findViewById(R.id.button_white_on);
-        buttonYellowOff = findViewById(R.id.button_yellow_off);
-        buttonBlueOff = findViewById(R.id.button_blue_off);
-        buttonPurpleOff = findViewById(R.id.button_purple_off);
-        buttonRedOff = findViewById(R.id.button_red_off);
+        buttonWhiteOn = findViewById(R.id.radio_button_white);
+        buttonYellowOff = findViewById(R.id.radio_button_yellow);
+        buttonBlueOff = findViewById(R.id.radio_button_blue);
+        buttonPurpleOff = findViewById(R.id.radio_button_purple);
+        buttonRedOff = findViewById(R.id.radio_button_red);
         buttonOn = findViewById(R.id.button_on);
 
-
-        buttonWhiteOn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fonColor = 1;
-                buttonWhiteOn.setImageResource(R.drawable.button_white_on);
-                buttonYellowOff.setImageResource(R.drawable.button_yellow_off);
-                buttonBlueOff.setImageResource(R.drawable.button_blue_off);
-                buttonPurpleOff.setImageResource(R.drawable.button_purple_off);
-                buttonRedOff.setImageResource(R.drawable.button_red_off);
-            }
-        });
-
-        buttonYellowOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fonColor = 2;
-                buttonYellowOff.setImageResource(R.drawable.button_yellow_on);
-                buttonWhiteOn.setImageResource(R.drawable.button_white_off);
-                buttonBlueOff.setImageResource(R.drawable.button_blue_off);
-                buttonPurpleOff.setImageResource(R.drawable.button_purple_off);
-                buttonRedOff.setImageResource(R.drawable.button_red_off);
-            }
-        });
-
-        buttonBlueOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fonColor = 3;
-                buttonBlueOff.setImageResource(R.drawable.button_blue_on);
-                buttonYellowOff.setImageResource(R.drawable.button_yellow_off);
-                buttonPurpleOff.setImageResource(R.drawable.button_purple_off);
-                buttonRedOff.setImageResource(R.drawable.button_red_off);
-                buttonWhiteOn.setImageResource(R.drawable.button_white_off);
-            }
-        });
-
-        buttonPurpleOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fonColor = 4;
-                buttonPurpleOff.setImageResource(R.drawable.button_purple_on);
-                buttonBlueOff.setImageResource(R.drawable.button_blue_off);
-                buttonYellowOff.setImageResource(R.drawable.button_yellow_off);
-                buttonRedOff.setImageResource(R.drawable.button_red_off);
-                buttonWhiteOn.setImageResource(R.drawable.button_white_off);
-            }
-        });
-
-        buttonRedOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fonColor = 5;
-                buttonRedOff.setImageResource(R.drawable.button_red_on);
-                buttonPurpleOff.setImageResource(R.drawable.button_purple_off);
-                buttonBlueOff.setImageResource(R.drawable.button_blue_off);
-                buttonYellowOff.setImageResource(R.drawable.button_yellow_off);
-                buttonWhiteOn.setImageResource(R.drawable.button_white_off);
-            }
-        });
 
         buttonOn.setOnClickListener(new View.OnClickListener() {
             @Override
